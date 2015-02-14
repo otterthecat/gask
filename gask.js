8b5aeaed99823ff585855929f0e6ab35c0e94b6e
Otter <otc@otterthecat.com>
2015-02-14 17:47:49 -0600

    updated regex and script will create gulp/tasks/ directory if it does not exist

--- gask.js
+++ gask.js
@@ -12,7 +12,7 @@ var blacklist = {
 
 // location of all available gulp tasks
 // EDIT AS REQUIRED FOR YOUR SYSTEM/SET UP
-var taskPath = '/home/user/path/to/gulp/tasks/';
+var taskPath = '/home/d/projects/github/big-gulp/gulp/tasks/';
 
 // arguments passed to script start at (zero indexed) 2nd key
 // if no args passed, default to Browserify
@@ -22,9 +22,7 @@ var files = args.length > 0 ? args : ['browserify'];
 // array to contain all required dev dependencies
 var taskList = [];
 
-// patterns to extract module names to be added to package.json
-var regexReq = /require\((\'|\")[a-zA-Z-_]+/g
-var regexMod = /require\((\'|\")/
+var regexReq = /[^require\(\'\"][a-z0-9_-]+(?=[\'\"]\);$)/gim
 
 // function to remove any duplicates from an array of strings
 var flatten = function(arry){
@@ -37,27 +35,61 @@ var flatten = function(arry){
 	});
 };
 
-// copy files & update taskList
-files.forEach(function(item){
-	var content = fs.readFileSync(taskPath + item + '.js', 'utf8');
-	// todo - check if /gulp/tasks/ directories exist
-	// if not, create them
-	fs.writeFileSync(process.cwd() + '/gulp/tasks/' + item + '.js', content, 'utf8');
-	taskList = taskList.concat(content.match(regexReq).map(function(item){
-		return item.replace(regexMod, '');
-	}));
-});
+var directoryPath = process.cwd() + '/gulp/tasks/';
 
-console.log('please wait while I install dev dependencies (I may need your password for this)');
+var getDependencies = function(){
+	// copy files & update taskList
+	files.forEach(function(item){
+		var content = fs.readFileSync(taskPath + item + '.js', 'utf8');
+		// todo - check if /gulp/tasks/ directories exist
+		// if not, create them
+		fs.writeFileSync(directoryPath + item + '.js', content, 'utf8');
+		taskList = taskList.concat(content.match(regexReq).map(function(item){
 
-// update package.json and install dev dependencies.
-// Notify user of success/fail
-exec('sudo npm install ' + flatten(taskList).join(' ') + ' --save-dev', function(err, stdout, stderr){
-	if(err !== null){
-		console.log('Dammit! This just happened:\n', err);
-		console.log(stderr);
-		return false;
+			// only return installed modules
+			if(item.indexOf('/') < 0){
+				return item;
+			}
+		}));
+	});
+};
+
+var installDependencies = function(){
+	console.log('please wait while I install dev dependencies (I may need your password for this)');
+
+	// update package.json and install dev dependencies.
+	// Notify user of success/fail
+	exec('sudo npm install ' + flatten(taskList).join(' ') + ' --save-dev', function(err, stdout, stderr){
+		if(err !== null){
+			console.log('Dammit! This just happened:\n', err);
+			console.log(stderr);
+			return false;
+		}
+
+		console.log('Gulp tasks added and package.json updated! Good for you!');
+	});
+}
+
+fs.stat(directoryPath, function(err, data){
+	if(err){
+
+		if(err.code === 'ENOENT') {
+			console.log('hmmm, seems you don\'t have a task directory set. I\'ll take care of it');
+			exec('mkdir -p ' + directoryPath, function(err){
+				if(err){
+					console.log('Dammit, I failed ', err);
+					return err;
+				}
+				console.log('Completed task directory');
+				getDependencies();
+				installDependencies();
+			});
+		}
+		else {
+			getDependencies();
+			installDependencies();
+		}
 	}
 
-	console.log('Gulp tasks added and package.json updated! Good for you!');
 });
+
